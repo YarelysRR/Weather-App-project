faea4e48ca78485366b049c4858483bae1dc83c4
Yarelys Rivera <yarelysrr@gmail.com>
2021-09-19 19:45:53 -0700

    Updated Html and Css
    New layout and changes in CSS. Still having issues with responsiveness.

--- src/index.js
+++ src/index.js
@@ -32,21 +32,11 @@ timeDayNow.innerHTML = showDateTime();
 
 // Search City= User input is display as City Name
 
-function search(city) {
-  let apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(getTemp);
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
+function searchCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-search");
   let cityName = document.querySelector("#city_name");
+
   if (city.value) {
     cityName.innerHTML = `${city.value}`;
     search(city.value);
@@ -54,8 +44,19 @@ function handleSubmit(event) {
     cityName.innerHTML = null;
     alert("Please enter a city");
   }
-  search(city);
 }
+
+function search(city) {
+  let units = "imperial";
+  let apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(getTemp);
+}
+
+let search_form = document.querySelector("#search-form");
+search_form.addEventListener("submit", searchCity);
 
 // Temperature Convertion
 
@@ -77,7 +78,7 @@ fahrenheitlink.addEventListener("click", convertToF);
 let celsiuslink = document.querySelector("#celsius_link");
 celsiuslink.addEventListener = ("click", convertToC);
 
-// What displays the city temperature
+// What displays the city weather info
 
 function getTemp(response) {
   console.log(response);
@@ -85,15 +86,41 @@ function getTemp(response) {
   let temperature = Math.round(response.data.main.temp);
   let tempmax = Math.round(response.data.main.temp_max);
   let tempmin = Math.round(response.data.main.temp_min);
+  let morning = response.data.sys.sunrise;
+  let dawn = response.data.sys.sunset;
+  let description = response.data.weather[0].description;
+  let humidity = Math.round(response.data.main.humidity);
+  let wind = Math.round(response.data.wind.speed);
   let city_temp = document.querySelector("#city_temp");
   let temp_high = document.querySelector("#temp_max");
   let temp_lo = document.querySelector("#temp_min");
   let cityName = document.querySelector("#city_name");
+  let sunrise = document.querySelector(".morning");
+  let sunset = document.querySelector(".dawn");
+  let humid = document.querySelector(".humidity");
+  let windy = document.querySelector(".wind");
+  let desc = document.querySelector(".description");
 
   city_temp.innerHTML = `${temperature}°F`;
   temp_high.innerHTML = `H: ${tempmax}°F`;
   temp_lo.innerHTML = `L: ${tempmin}°F`;
   cityName.innerHTML = `${city}`;
+  sunrise.innerHTML = ` Sunrise:${morning}`;
+  sunset.innerHTML = ` Sunset:${dawn}`;
+  humid.innerHTML = `${humidity}`;
+  windy.innerHTML = ` ${wind}`;
+  desc.innerHTML = ` ${description}`;
+}
+
+function GetCurrentLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "imperial";
+  let apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(getTemp);
 }
 
 function getPosition(event) {
@@ -101,19 +128,8 @@ function getPosition(event) {
   navigator.geolocation.getCurrentPosition(GetCurrentLocation);
 }
 
-function GetCurrentLocation(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "8d3b4eb3bfd4da849a5a61c1e36fe700";
-  let endpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${endpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(getTemp);
-}
-
 let currentLocationButton = document.querySelector("#currentButton");
 currentLocationButton.addEventListener("click", getPosition);
 
 let searchLocationButton = document.querySelector("#searchButton");
-searchLocationButton.addEventListener("click", handleSubmit);
-
-search("San Juan");
+searchLocationButton.addEventListener("click", searchCity);
